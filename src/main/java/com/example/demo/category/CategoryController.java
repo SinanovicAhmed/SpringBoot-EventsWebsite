7d0a27b04d69c6b2cd8ca4e53f4fed8fe448b769
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2022-11-16 23:07:20 +0100

    Categories endpoint added

---
+++ src/main/java/com/example/demo/category/CategoryController.java
@@ -0,0 +1,39 @@
+package com.example.demo.category;
+import com.example.demo.location.Location;
+import com.example.demo.location.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path= "api/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Autowired
+    @GetMapping
+    public ResponseEntity<List<Category>> getCategories(){
+        return categoryService.getLocations();
+    }
+    @GetMapping(value = "{id}")
+    public ResponseEntity<?> getCategoryById(@PathVariable("id") Long id){
+        return categoryService.getCategoryById(id);
+    }
+    @ResponseBody
+    @PostMapping(value = "/save")
+    public ResponseEntity<String> postCategory(@RequestBody Category category){
+        return categoryService.addNewLocation(category);
+    }
+
+    @PutMapping(value = "/updateCategory/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable("id") Long id,
+                                                 @RequestBody Category category){
+        return categoryService.updateCategory(id, category);
+    }
+}
